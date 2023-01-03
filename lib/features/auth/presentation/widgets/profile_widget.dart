9b73d78510d37810f69aceb2d8227cadf6752055
osamasabry9 <osamasabry324@gmail.com>
2023-01-03 14:41:36 +0200

    profile Widget

---
+++ lib/features/auth/presentation/widgets/profile_widget.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/utils/assets_manager.dart';
+
+Widget profileWidget({String? imageUrl, File? image}) {
+  if (image == null) {
+    if (imageUrl == null || imageUrl == "") {
+      return Image.asset(
+        ImageAssets.profileDefault,
+        fit: BoxFit.cover,
+      );
+    } else {
+      return CachedNetworkImage(
+        imageUrl: imageUrl,
+        fit: BoxFit.cover,
+        progressIndicatorBuilder: (context, url, downloadProgress) {
+          return const CircularProgressIndicator();
+        },
+        errorWidget: (context, url, error) => Image.asset(
+          ImageAssets.profileDefault,
+          fit: BoxFit.cover,
+        ),
+      );
+    }
+  } else {
+    return Image.file(image, fit: BoxFit.cover,);
+  }
+}
