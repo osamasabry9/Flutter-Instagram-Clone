ce7084cc4ebd0bb606c2f842e240c62fe7e56c97
osamasabry9 <osamasabry324@gmail.com>
2023-01-04 16:12:58 +0200

    profile screen

---
+++ lib/features/profile/presentation/screen/profile_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/utils/values_manager.dart';
+
+import '../widgets/app_bar_profile_widget.dart';
+import '../widgets/info_user_widget.dart';
+import '../widgets/view_posts_widget.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarProfileWidget(),
+      body: Padding(
+        padding: const EdgeInsets.all(AppPadding.p14),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const InfoUserWidget(),
+              const SizedBox(height: AppSize.s12),
+              Text(
+                "Name",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: AppSize.s12),
+              const Text(
+                "The bio of user.",
+              ),
+              const SizedBox(height: AppSize.s20),
+              const ViewPostsWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
