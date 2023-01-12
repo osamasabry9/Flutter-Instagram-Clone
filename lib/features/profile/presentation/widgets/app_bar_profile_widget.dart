f51420f2a4ae59fe71299d215d2316c6db42ce61
osamasabry9 <osamasabry324@gmail.com>
2023-01-12 04:21:11 +0200

    Profile Widgets

--- lib/features/profile/presentation/widgets/app_bar_profile_widget.dart
+++ lib/features/profile/presentation/widgets/app_bar_profile_widget.dart
@@ -1,17 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/utils/routes_manager.dart';
 import '../../../../../core/utils/values_manager.dart';
+import '../../../../core/utils/color_manager.dart';
+import '../../../../core/utils/constants_manager.dart';
 
-AppBar appBarProfileWidget() {
+AppBar appBarProfileWidget(
+  BuildContext context,
+) {
   return AppBar(
     automaticallyImplyLeading: false,
     title: const Text("User Name"),
-    actions: const [
+    actions: [
       Padding(
-        padding: EdgeInsets.only(right: AppPadding.p12),
-        child: Icon(
-          Icons.menu,
+        padding: const EdgeInsets.only(right: AppPadding.p12),
+        child: InkWell(
+          onTap: () {
+            _openBottomModalSheet(context);
+          },
+          child: const Icon(
+            Icons.menu,
+          ),
         ),
       )
     ],
   );
 }
+
+_openBottomModalSheet(
+  BuildContext context,
+) {
+  return showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          height: 150,
+          decoration:
+              BoxDecoration(color: ColorManager.darkGrey.withOpacity(.8)),
+          child: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      "More Options",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: ColorManager.white),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Divider(
+                    thickness: 1,
+                    color: ColorManager.black,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, Routes.editProfileRoute);
+                      },
+                      child: const Text(
+                        "Edit Profile",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                            color: ColorManager.white),
+                      ),
+                    ),
+                  ),
+                  AppConstants.sizeVer(AppSize.s8),
+                  const Divider(
+                    thickness: 1,
+                    color: ColorManager.black,
+                  ),
+                  AppConstants.sizeVer(AppSize.s8),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: GestureDetector(
+                      onTap: _logOut,
+                      child: const Text(
+                        "Log Out",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                            color: ColorManager.white),
+                      ),
+                    ),
+                  ),
+                  AppConstants.sizeVer(AppSize.s8),
+                ],
+              ),
+            ),
+          ),
+        );
+      });
+}
+
+
+_logOut() {}
