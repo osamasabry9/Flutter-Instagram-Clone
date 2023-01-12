5d34045085e706dc36fd438f4be35eca8714ed13
osamasabry9 <osamasabry324@gmail.com>
2023-01-12 04:20:19 +0200

    Post Pages

---
+++ lib/features/Post/presentation/screen/update_post_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/update_post_main_widget.dart';
+
+class UpdatePostPage extends StatelessWidget {
+  const UpdatePostPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const UpdatePostMainWidget();
+  }
+}
