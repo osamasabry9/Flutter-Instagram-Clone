4277d552e6dd36a7cf0afa3dc9f1180d4b4b553b
osamasabry9 <osamasabry324@gmail.com>
2023-01-13 04:49:03 +0200

    Some edits in Profile screens and widgets

---
+++ lib/features/user/presentation/profile/widgets/input_edit_profile_widget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../core/utils/color_manager.dart';
+import '../../../../../../core/utils/values_manager.dart';
+
+class InputEditProfileWidget extends StatelessWidget {
+  final String label;
+  final bool isProfile;
+
+  final TextEditingController textController;
+
+  const InputEditProfileWidget({
+    Key? key,
+    required this.label,
+    required this.textController,
+    this.isProfile = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: AppPadding.p16,
+            vertical: AppPadding.p8,
+          ),
+          child: Row(
+            children: [
+              if (isProfile)
+                Text(
+                  label,
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                        fontSize: AppSize.s16,
+                        color: ColorManager.grey,
+                      ),
+                ),
+              if (isProfile)
+                const SizedBox(
+                  width: AppSize.s14,
+                ),
+              Expanded(
+                child: TextFormField(
+                  controller: textController,
+                  textAlign: isProfile ? TextAlign.end : TextAlign.start,
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                        fontSize: AppSize.s16,
+                        color: ColorManager.white,
+                      ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return '$label must be filled';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    fillColor: ColorManager.background,
+                    label: Text(
+                      isProfile ? "" : label,
+                    ),
+                    labelStyle:
+                        Theme.of(context).textTheme.headlineMedium!.copyWith(
+                              fontSize: AppSize.s16,
+                              color: ColorManager.white,
+                            ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Divider(
+          color: ColorManager.grey1,
+          height: 1,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/features/user/presentation/profile/widgets/view_posts_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/widgets/image_profile_widget.dart';
+
+class ViewPostsWidget extends StatelessWidget {
+  const ViewPostsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 5,
+      physics: const ScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+      ),
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {},
+          child: SizedBox(
+            width: 100,
+            height: 100,
+            child: imageProfileWidget(imageUrl: ''),
+          ),
+        );
+      },
+    );
+  }
+}
