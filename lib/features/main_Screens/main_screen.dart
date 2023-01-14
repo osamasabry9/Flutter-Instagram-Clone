62b62d4e7e79b7bafcd6d9dc63f782e5c5a41678
osamasabry9 <osamasabry324@gmail.com>
2023-01-14 04:00:43 +0200

    add cubit in main Screen

--- lib/features/main_Screens/main_screen.dart
+++ lib/features/main_Screens/main_screen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:icons_plus/icons_plus.dart';
 
 import '../../core/utils/color_manager.dart';
 import '../Post/presentation/screen/post_screen.dart';
+import '../user/presentation/profile/cubit/get_single_user/get_single_user_cubit.dart';
 import '../user/presentation/profile/screen/profile_screen.dart';
 import 'Screens/activity/screen/activity_screen.dart';
 import 'Screens/home/screen/home_screen.dart';
 import 'Screens/search/screen/search_screen.dart';
 
-
 class MainScreen extends StatefulWidget {
+  final String uid;
   const MainScreen({
     Key? key,
+    required this.uid,
   }) : super(key: key);
 
   @override
@@ -32,6 +35,7 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   void initState() {
+    BlocProvider.of<GetSingleUserCubit>(context).getSingleUser(uid: widget.uid);
     pageController = PageController();
     super.initState();
   }
@@ -48,34 +52,48 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: CupertinoTabBar(
-        backgroundColor: ColorManager.black,
-        activeColor: ColorManager.white,
-        inactiveColor: ColorManager.grey,
-        currentIndex: _currentIndex,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Bootstrap.house_fill), label: ""),
-          BottomNavigationBarItem(icon: Icon(Bootstrap.search), label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(Bootstrap.plus_circle_fill), label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle_outlined), label: ""),
-        ],
-        onTap: navigationTapped,
-      ),
-      body: PageView(
-        controller: pageController,
-        onPageChanged: onPageChanged,
-        children: const [
-          HomeScreen(),
-          SearchScreen(),
-          PostScreen(),
-          ActivityScreen(),
-          ProfileScreen(),
-        ],
-      ),
+    return BlocBuilder<GetSingleUserCubit, GetSingleUserState>(
+      builder: (context, getSingleUserState) {
+        if (getSingleUserState is GetSingleUserLoaded) {
+          final currentUser = getSingleUserState.user;
+          return Scaffold(
+            bottomNavigationBar: CupertinoTabBar(
+              backgroundColor: ColorManager.black,
+              activeColor: ColorManager.white,
+              inactiveColor: ColorManager.grey,
+              currentIndex: _currentIndex,
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(Bootstrap.house_fill), label: ""),
+                BottomNavigationBarItem(
+                    icon: Icon(Bootstrap.search), label: ""),
+                BottomNavigationBarItem(
+                    icon: Icon(Bootstrap.plus_circle_fill), label: ""),
+                BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.account_circle_outlined), label: ""),
+              ],
+              onTap: navigationTapped,
+            ),
+            body: PageView(
+              controller: pageController,
+              onPageChanged: onPageChanged,
+              children:  [
+                const HomeScreen(),
+                const SearchScreen(),
+                const PostScreen(),
+                const ActivityScreen(),
+                ProfileScreen(currentUser: currentUser),
+              ],
+            ),
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(
+            color: ColorManager.primary,
+          ),
+        );
+      },
     );
   }
 }
