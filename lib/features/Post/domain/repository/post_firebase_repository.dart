1892d7b0e25dffbc7fa592463dd3e775487557a8
osamasabry9 <osamasabry324@gmail.com>
2023-01-17 03:10:33 +0200

    add Domain layer in post

---
+++ lib/features/Post/domain/repository/post_firebase_repository.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+import '../entities/post_entity.dart';
+
+abstract class PostFirebaseRepository {
+    // Cloud Storage Feature
+  Future<String> uploadPostImageToStorage(File? file);
+
+  // Post Features
+  Future<void> createPost(PostEntity post);
+  Stream<List<PostEntity>> readPosts(PostEntity post);
+  Stream<List<PostEntity>> readSinglePost(String postId);
+  Future<void> updatePost(PostEntity post);
+  Future<void> deletePost(PostEntity post);
+  Future<void> likePost(PostEntity post);
+}
