11dfc8f73dd71e657bb3f434ad9cfed390985603
osamasabry9 <osamasabry324@gmail.com>
2023-01-20 07:39:28 +0200

    Activate search and link it with the firebase

--- lib/features/main_Screens/Screens/search/screen/search_screen.dart
+++ lib/features/main_Screens/Screens/search/screen/search_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram_clone/features/Post/presentation/cubit/post_cubit.dart';
 
 import '../widgets/search_main_widget.dart';
+import '../../../../../app/di.dart' as di;
+
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SearchMainWidget();
+    return  BlocProvider<PostCubit>(
+      create: (context) =>di.instance<PostCubit>(),
+      child: const SearchMainWidget(),
+    );
   }
 }
--- lib/features/main_Screens/Screens/search/widgets/search_main_widget.dart
+++ lib/features/main_Screens/Screens/search/widgets/search_main_widget.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram_clone/core/utils/color_manager.dart';
+import 'package:instagram_clone/features/Post/domain/entities/post_entity.dart';
+import 'package:instagram_clone/features/user/presentation/profile/cubit/user_cubit.dart';
 
 import '../../../../../core/utils/constants_manager.dart';
+import '../../../../../core/utils/routes_manager.dart';
 import '../../../../../core/utils/values_manager.dart';
 import '../../../../../core/widgets/image_profile_widget.dart';
 import '../../../../../core/widgets/input_field.dart';
+import '../../../../Post/presentation/cubit/post_cubit.dart';
+import '../../../../user/domain/entities/user_entity.dart';
 
 class SearchMainWidget extends StatefulWidget {
   const SearchMainWidget({Key? key}) : super(key: key);
@@ -23,6 +30,8 @@ class _SearchMainWidgetState extends State<SearchMainWidget> {
 
   @override
   void initState() {
+    BlocProvider.of<UserCubit>(context).getUsers(user: const UserEntity());
+    BlocProvider.of<PostCubit>(context).getPosts(post: const PostEntity());
     super.initState();
     _searchController.addListener(() {
       setState(() {});
@@ -32,73 +41,122 @@ class _SearchMainWidgetState extends State<SearchMainWidget> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            InputField(
-              textController: _searchController,
-              label: "Search",
-              hint: "Search",
-              prefix: Icons.search,
-              validate: (p0) {
-                return null;
-              },
-            ),
-            AppConstants.sizeVer(AppSize.s20),
-            _searchController.text.isNotEmpty
-                ? Expanded(
-                    child: ListView.builder(
-                        itemCount: 3,
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                            onTap: () {},
-                            child: Row(
-                              children: [
-                                Container(
-                                  margin:
-                                      const EdgeInsets.symmetric(vertical: 10),
-                                  width: 40,
-                                  height: 40,
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(20),
-                                    child: imageProfileWidget(imageUrl: ''),
+      child: BlocBuilder<UserCubit, UserState>(
+        builder: (context, userState) {
+          if (userState is UserLoaded) {
+            final filterAllUsers = userState.users
+                .where((user) =>
+                    user.username!.startsWith(_searchController.text) ||
+                    user.username!
+                        .toLowerCase()
+                        .startsWith(_searchController.text.toLowerCase()) ||
+                    user.username!.contains(_searchController.text) ||
+                    user.username!
+                        .toLowerCase()
+                        .contains(_searchController.text.toLowerCase()))
+                .toList();
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InputField(
+                    textController: _searchController,
+                    label: "Search",
+                    hint: "Search",
+                    prefix: Icons.search,
+                    validate: (p0) {
+                      return null;
+                    },
+                  ),
+                  AppConstants.sizeVer(AppSize.s20),
+                  _searchController.text.isNotEmpty
+                      ? Expanded(
+                          child: ListView.builder(
+                              itemCount: filterAllUsers.length,
+                              itemBuilder: (context, index) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    Navigator.pushNamed(
+                                        context, Routes.singleUserProfileRoute,
+                                        arguments: filterAllUsers[index].uid);
+                                  },
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        margin: const EdgeInsets.symmetric(
+                                            vertical: AppMargin.m12),
+                                        width: AppSize.s40,
+                                        height: AppSize.s40,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(
+                                              AppSize.s20),
+                                          child: imageProfileWidget(
+                                              imageUrl: filterAllUsers[index]
+                                                  .profileUrl),
+                                        ),
+                                      ),
+                                      AppConstants.sizeHor(AppSize.s12),
+                                      Text(
+                                        "${filterAllUsers[index].username}",
+                                        style: const TextStyle(
+                                            color: ColorManager.white,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w600),
+                                      )
+                                    ],
                                   ),
-                                ),
-                                AppConstants.sizeHor(AppSize.s12),
-                                const Text(
-                                  "username",
-                                )
-                              ],
-                            ),
-                          );
-                        }),
-                  )
-                : Expanded(
-                    child: GridView.builder(
-                        itemCount: 2,
-                        physics: const ScrollPhysics(),
-                        shrinkWrap: true,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          crossAxisSpacing: 5,
-                          mainAxisSpacing: 5,
-                        ),
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                            onTap: () {},
-                            child: SizedBox(
-                              width: 100,
-                              height: 100,
-                              child: imageProfileWidget(imageUrl: ''),
-                            ),
-                          );
-                        }),
-                  )
-          ],
-        ),
+                                );
+                              }),
+                        )
+                      : Expanded(
+                          child: BlocBuilder<PostCubit, PostState>(
+                            builder: (context, postState) {
+                              if (postState is PostLoaded) {
+                                final post = postState.posts;
+                                return GridView.builder(
+                                  itemCount: post.length,
+                                  physics: const ScrollPhysics(),
+                                  shrinkWrap: true,
+                                  gridDelegate:
+                                      const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 3,
+                                    crossAxisSpacing: 5,
+                                    mainAxisSpacing: 5,
+                                  ),
+                                  itemBuilder: (context, index) {
+                                    return GestureDetector(
+                                      onTap: () {
+                                        Navigator.pushNamed(
+                                            context, Routes.postDetailRoute,
+                                            arguments: post[index].postId);
+                                      },
+                                      child: SizedBox(
+                                        width: 100,
+                                        height: 100,
+                                        child: imageProfileWidget(
+                                            imageUrl: post[index].postImageUrl),
+                                      ),
+                                    );
+                                  },
+                                );
+                              }
+                              return const Center(
+                                child: CircularProgressIndicator(
+                                    color: ColorManager.primary),
+                              );
+                            },
+                          ),
+                        )
+                ],
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(color: ColorManager.primary),
+          );
+        },
       ),
     );
   }
