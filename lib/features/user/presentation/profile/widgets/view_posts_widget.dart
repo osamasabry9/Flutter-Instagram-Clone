321e73d5c9e208a6f6e692013170d82fa24d91f2
osamasabry9 <osamasabry324@gmail.com>
2023-01-20 07:41:19 +0200

    Show posts in profile screen

--- lib/features/user/presentation/profile/widgets/view_posts_widget.dart
+++ lib/features/user/presentation/profile/widgets/view_posts_widget.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram_clone/core/utils/routes_manager.dart';
+import 'package:instagram_clone/features/Post/presentation/cubit/post_cubit.dart';
 import '../../../../../core/widgets/image_profile_widget.dart';
 
 class ViewPostsWidget extends StatelessWidget {
-  const ViewPostsWidget({super.key});
+  final String userId;
+  const ViewPostsWidget({super.key, required this.userId});
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 5,
-      physics: const ScrollPhysics(),
-      shrinkWrap: true,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 5,
-        mainAxisSpacing: 5,
-      ),
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {},
-          child: SizedBox(
-            width: 100,
-            height: 100,
-            child: imageProfileWidget(imageUrl: ''),
-          ),
+    return BlocBuilder<PostCubit, PostState>(
+      builder: (context, postState) {
+        if (postState is PostLoaded) {
+          final posts = postState.posts
+              .where((post) => post.creatorUid == userId)
+              .toList();
+          return GridView.builder(
+              itemCount: posts.length,
+              physics: const ScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, crossAxisSpacing: 5, mainAxisSpacing: 5),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, Routes.postDetailRoute,
+                        arguments: posts[index].postId);
+                  },
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child:
+                        imageProfileWidget(imageUrl: posts[index].postImageUrl),
+                  ),
+                );
+              });
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
         );
       },
     );
