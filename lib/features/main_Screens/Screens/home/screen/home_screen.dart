3253864a3259a3567447992f57cc03c01cfdc564
osamasabry9 <osamasabry324@gmail.com>
2023-01-30 14:19:36 +0200

    add story in home screen

--- lib/features/main_Screens/Screens/home/screen/home_screen.dart
+++ lib/features/main_Screens/Screens/home/screen/home_screen.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:instagram_clone/core/utils/color_manager.dart';
+import '../../../../../core/utils/color_manager.dart';
 
-import 'package:instagram_clone/core/utils/constants_manager.dart';
-import 'package:instagram_clone/core/utils/values_manager.dart';
+import '../../../../../core/utils/constants_manager.dart';
+import '../../../../../core/utils/values_manager.dart';
 
 import '../../../../../core/utils/routes_manager.dart';
 import '../../../../Post/domain/entities/post_entity.dart';
 import '../../../../Post/presentation/cubit/post_cubit.dart';
+import '../../../../stroy/presentation/widgets/story_data.dart';
 import '../../../../user/domain/entities/user_entity.dart';
+import '../../../../user/presentation/profile/cubit/user_cubit.dart';
 import '../widgets/app_bar_widget.dart';
 import '../widgets/no_posts_yet_widget.dart';
 import '../widgets/single_post_card_widget.dart';
 import '../../../../../app/di.dart' as di;
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.currentUser});
 
   final UserEntity currentUser;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    BlocProvider.of<UserCubit>(context).getUsers(user: const UserEntity());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,69 +38,12 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(AppPadding.p10),
-          child: Column(
-            children: [
-              appBarRowWidget(context),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.16,
-                width: double.infinity,
-                padding: const EdgeInsets.all(AppPadding.p14),
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (context, index) => GestureDetector(
-                    onTap: () => index != 0
-                        ? Navigator.pushNamed(
-                            context,
-                            Routes.storyRoute,
-                          )
-                        : null,
-                    child: Column(
-                      children: [
-                        Stack(
-                          alignment: AlignmentDirectional.bottomEnd,
-                          children: [
-                            CircleAvatar(
-                              radius: AppSize.s32,
-                              backgroundColor:
-                                  index != 0 ? ColorManager.error : null,
-                              child: CircleAvatar(
-                                radius: AppSize.s30,
-                                backgroundImage:
-                                    NetworkImage("${currentUser.profileUrl}"),
-                              ),
-                            ),
-                            if (index == 0)
-                              const CircleAvatar(
-                                radius: 16.0,
-                                child: Icon(
-                                  Icons.camera_alt,
-                                  size: 16,
-                                ),
-                              ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: AppSize.s8,
-                        ),
-                        Text(
-                          index != 0 ? "username" : "Your story",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .copyWith(color: ColorManager.white),
-                          maxLines: 1,
-                        )
-                      ],
-                    ),
-                  ),
-                  separatorBuilder: (context, index) => const SizedBox(
-                    width: AppSize.s8,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: BlocProvider<PostCubit>(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                appBarRowWidget(context),
+                storyWidget(context),
+                BlocProvider<PostCubit>(
                   create: (context) => di.instance<PostCubit>()
                     ..getPosts(post: const PostEntity()),
                   child: BlocBuilder<PostCubit, PostState>(
@@ -106,6 +62,8 @@ class HomeScreen extends StatelessWidget {
                             ? const NoPostsYetWidget()
                             : ListView.builder(
                                 itemCount: postState.posts.length,
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
                                 itemBuilder: (context, index) {
                                   final post = postState.posts[index];
                                   return BlocProvider(
@@ -122,10 +80,94 @@ class HomeScreen extends StatelessWidget {
                     },
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget storyWidget(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.16,
+      width: double.infinity,
+      padding: const EdgeInsets.all(AppPadding.p14),
+      child: BlocBuilder<UserCubit, UserState>(
+        builder: (context, userState) {
+          if (userState is UserLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (userState is UserFailure) {
+            AppConstants.toast(
+                "Some Failure occurred while creating the GetMyChat");
+          }
+          if (userState is UserLoaded) {
+            
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: userState.users.length,
+              itemBuilder: (context, index) =>
+                  itemUserStory(context, userState.users[index]),
+              separatorBuilder: (context, index) => const SizedBox(
+                width: AppSize.s8,
+              ),
+            );
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+
+  GestureDetector itemUserStory(BuildContext context, UserEntity user) {
+    return GestureDetector(
+      onTap: () => user.uid != widget.currentUser.uid
+          ? Navigator.pushNamed(
+              context,
+              Routes.storyRoute,
+            )
+          : null,
+      child: Column(
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: [
+              CircleAvatar(
+                radius: AppSize.s32,
+                backgroundColor: user.uid != widget.currentUser.uid
+                    ? ColorManager.error
+                    : null,
+                child: CircleAvatar(
+                    radius: AppSize.s30,
+                    backgroundImage: NetworkImage("${user.profileUrl}")),
+              ),
+              if (user.uid == widget.currentUser.uid)
+                const CircleAvatar(
+                  radius: 16.0,
+                  child: Icon(
+                    Icons.camera_alt,
+                    size: 16,
+                  ),
+                ),
+            ],
+          ),
+          const SizedBox(
+            height: AppSize.s8,
+          ),
+          Text(
+            user.uid != widget.currentUser.uid
+                ? "${user.username}"
+                : "Your story",
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(color: ColorManager.white),
+            maxLines: 1,
+          )
+        ],
       ),
     );
   }
